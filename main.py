c76b68df93b373e5d2b93f3a491f753e56df02d5
frankjungblut <franksjungblut@gmail.com>
2023-12-18 15:39:50 -0300

    Melhorias
    -Os elementos da janela são redimensionado automaticamente
    -o teclado pode ser utilizado para inserir valores

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Calculadora")
-
-display = tk.Entry(root, width=35, borderwidth=5)
-display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
-
-buttons = [
-    '7', '8', '9', '+',
-    '4', '5', '6', '-',
-    '1', '2', '3', '*',
-    'C', '0', '=', '/'
-]
-
-row_val = 1
-col_val = 0
-
 def on_click(char):
     current_text = display.get()
     display.delete(0, tk.END)
@@ -33,13 +17,43 @@ def calculate():
 def clear_display():
     display.delete(0, tk.END)
 
+def on_key_press(event):
+    char = event.char
+    if char.isdigit() or char in ['+', '-', '*', '/']:
+        current_text = display.get()
+        # Verifica se o último caractere já é o que está sendo inserido para evitar duplicação
+        if not current_text.endswith(char):
+            on_click(char)
+    elif event.keysym == 'Return':
+        calculate()
+    elif event.keysym in ['BackSpace', 'Delete']:
+        clear_display()
+
+janela = tk.Tk()
+janela.title("Calculadora")
+janela.bind('<Key>', on_key_press)
+
+display = tk.Entry(janela, width=35, borderwidth=5)
+display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+display.focus_set()  # Foco no display para capturar os eventos do teclado
+
+buttons = [
+    '7', '8', '9', '+',
+    '4', '5', '6', '-',
+    '1', '2', '3', '*',
+    'C', '0', '=', '/'
+]
+
+row_val = 1
+col_val = 0
+
 for button in buttons:
-    tk.Button(root, text=button, padx=40, pady=20, command=lambda b=button: on_click(b) if b not in ['=', 'C'] else calculate() if b == '=' else clear_display()).grid(row=row_val, column=col_val)
+    b = tk.Button(janela, text=button, padx=40, pady=20, command=lambda b=button: on_click(b) if b not in ['=', 'C'] else calculate() if b == '=' else clear_display())
+    b.grid(row=row_val, column=col_val, sticky='nsew')
     col_val += 1
     if col_val > 3:
         col_val = 0
         row_val += 1
+        janela.grid_rowconfigure(row_val, weight=1)
 
-# Loop principal
-root.mainloop()
-    
+janela.mainloop()
